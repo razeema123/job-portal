7c31dc28bef25e8fe8e4afef8176cdf5cbc7ed49
revathy667 <revathynaranathu@gmail.com>
2025-07-18 15:05:30 +0530

    side bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from "./pages/signup.jsx";
 import Login from "./pages/login.jsx";
+import Sidebar from './components/sidebar.jsx';
 
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        
+        <Route path="/" element={<Signup />} />
+        <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/dashboard" element={<h2>Welcome to Job Portal Dashboard</h2>} />
       </Routes>
     </Router>
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard() {
+  return <h1>📊 Dashboard Page</h1>;
+}
+
+export default Dashboard;

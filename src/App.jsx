9d82eeb07e81bbb31d77946bbe3054c969424982
Mahima1111 <mahimavijayan123@gmail.com>
2025-07-25 14:40:14 +0530

    applicationpages

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,25 @@ import Sidebar from './components/sidebar.jsx';
  
 import ProgressGraph from './components/progressgraph.jsx';
 import AdminLayout from "./pages/adminlayout.jsx";
+ 
+
+import CreateJob from './applications/createjob.jsx'; 
+
+
 import Home from './pages/home.jsx';  
 import FindJobs from './findjob.jsx';
 import ForgotPassword from './forgott.jsx';
 import ApplyJob from './applyjob.jsx';
-import CreateJob from './applications/createjob.jsx';
 import PostJob from './applications/postjob.jsx';
 import ViewApplications from './applications/ViewApplications.jsx';
+
+
+
+
 import ViewProfile from './pages/viewprofile.jsx';
+
+
+
 
 function App() {
   return (
@@ -22,11 +33,16 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+ 
         <Route path="/admin" element={<Sidebar />} />
         <Route path="admin/dashboard" element={<h2>Welcome to Job Portal Dashboard</h2>} />
         <Route path="/admin/progressgraph" element={<ProgressGraph />} />
         <Route path="/admin/adminlayout" element={<AdminLayout />} />
-        <Route path="/sidebar" element={<Sidebar />} />
+       
+
+
+       <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/dashboard" element={<h2>Welcome to Job Portal Dashboard</h2>} />
         <Route path="/home" element={<Home />} />
         <Route path="/find-jobs" element={<FindJobs />} />
--- src/applications/postjob.jsx
+++ src/applications/postjob.jsx
@@ -52,8 +52,8 @@ export default function PostJob() {
       <div className="sidebar">
         <h2 className="sidebar-title">Job Portal</h2>
         <ul className="sidebar-menu">
-          <li><Link to="/postjob">Posted Job</Link></li>
-          <li><Link to="/createjob">Create Job</Link></li>
+          <li><Link to="/postjob"> Jobs</Link></li>
+          <li><Link to="/createjob">Applications</Link></li>
           
         </ul>
       </div>
